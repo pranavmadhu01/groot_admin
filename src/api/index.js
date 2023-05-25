75e6e5c1565b77fa7aa75c8d537042d645753dab
pranav madhu <pranavmadhu73@gmail.com>
2023-05-25 15:40:46 +0530

    Added plant apis for adding and getting all ; added PhotoCard for displaying photos along with name

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ const { default: axios } = require("axios");
 
 const url = process.env.BACKEND_URL || "http://localhost:8000";
 const fertilizerUrl = `${url}/fertilizer`;
+const plantUrl = `${url}/plant`;
 //fertilizer apis
 const getAllFertilzers = async () => {
   return await axios.get(fertilizerUrl);
@@ -10,3 +11,16 @@ const addFertilizer = async (data) => {
   return await axios.post(fertilizerUrl, data);
 };
 export { getAllFertilzers, addFertilizer };
+
+//plant apis
+const addANewPlant = async (data) => {
+  return await axios.post(plantUrl, data, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
+const getAllPlants = async () => {
+  return await axios.get(plantUrl);
+};
+export { addANewPlant, getAllPlants };
--- src/app/dashboard/plant/page.js
+++ src/app/dashboard/plant/page.js
@@ -1,18 +1,40 @@
 "use client";
+import { getAllPlants } from "@/api";
 import AddCard from "@/components/cards/AddCard/AddCard";
-import AddFertilizerCard from "@/components/cards/AddCard/AddCard";
+import PhotoCard from "@/components/cards/PhotoCard/PhotoCard";
 import AddPlantDrawer from "@/components/drawers/AddPlantDrawer/AddPlantDrawer";
 import { SimpleGrid } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { notifications } from "@mantine/notifications";
+import { useEffect, useState } from "react";
 
 export default function Plant() {
   const [opened, { open, close, toggle }] = useDisclosure(false);
-
+  const [plants, setPlants] = useState([]);
+  useEffect(() => {
+    getAllPlants()
+      .then((response) => {
+        console.log(response.data.data);
+        setPlants(response.data.data);
+      })
+      .catch((error) => {
+        notifications.show({
+          title: "Error",
+          message: error.response.data.message,
+        });
+      });
+  }, [opened]);
   return (
     <SimpleGrid cols={3}>
-      {/* {fertilizers.map(({ name, price, _id }) => (
-        <DisplayCard price={price} title={name} key={_id} />
-      ))} */}
+      {plants.map(({ name, defaultph, image, seedrate, _id }) => (
+        <PhotoCard
+          defaultph={defaultph}
+          image={image}
+          name={name}
+          seedrate={seedrate}
+          key={_id}
+        />
+      ))}
       <AddPlantDrawer opened={opened} close={close} />
       <AddCard name="Add Plant" onClick={open} />
     </SimpleGrid>
